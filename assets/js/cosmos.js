114d175b1727e07b3d9129fb70ff34bfface89e9
houmin <weihoumin@gmail.com>
2022-12-26 18:28:50 +0800

    feat: rewrite back2top

--- assets/js/cosmos.js
+++ assets/js/cosmos.js
@@ -4,15 +4,21 @@ const Cosmos = {};
 
 Cosmos.back2Top = function() {
     const displayBack2Top = 200;
-    const back2Top = document.getElementById("top-link");
+    var back2top = document.getElementById("back2top");
     window.onscroll = function () {
         if (document.body.scrollTop > displayBack2Top || document.documentElement.scrollTop > displayBack2Top) {
-            back2Top.style.visibility = "visible";
-            back2Top.style.opacity = "1";
+            back2top.style.visibility = "visible";
+            back2top.style.opacity = "1";
         } else {
-            back2Top.style.visibility = "hidden";
-            back2Top.style.opacity = "0";
+            back2top.style.visibility = "hidden";
+            back2top.style.opacity = "0";
         }
+    }
+
+    back2top.onclick = function () {
+        // cancel if already on top
+        if (document.scrollingElement.scrollTop === 0) return;
+        document.scrollingElement.scrollTop = 0;
     }
 };
 
