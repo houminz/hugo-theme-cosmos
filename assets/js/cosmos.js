1eef3fa408fcfdb85048768b8cbf706d73d4cbb4
houmin <weihoumin@gmail.com>
2022-12-28 12:01:06 +0800

    feat: remove jQuery/bootstrap imported by tabs shortcode

--- assets/js/cosmos.js
+++ assets/js/cosmos.js
@@ -240,3 +240,29 @@ Cosmos.douban = function() {
         }
     });
 };
+
+Cosmos.tabs = function () {
+    var navTabs = document.querySelectorAll('#nav-tab>li');
+    for (var i = 0; i < navTabs.length; i++) {
+        navTabs[i].addEventListener("click", function(){
+            //  divs - removing .active class
+            var tabs = document.querySelectorAll('.tab-content>.tab-pane');
+            for (var k = 0; k < tabs.length; k++) {
+                tabs[k].className = "tab-pane";
+            }
+            // removing .active from menu items
+            for (var j = 0; j < navTabs.length; j++) {
+                navTabs[j].className = "";
+            }
+
+            // setting .active in clicked item
+            this.className = "active";
+            // getting target id
+            var linkTab = this.getElementsByTagName("A")[0].id;
+
+            // showing the selected tab div
+            var tab = document.querySelectorAll('.tab-content>#'+linkTab)[0];
+            tab.className = "tab-pane active";
+        });
+    };
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     Cosmos.back2top();
     Cosmos.toc();
     Cosmos.douban();
+    Cosmos.tabs();
 });
