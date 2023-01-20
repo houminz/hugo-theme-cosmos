9c4c30fcd7209a4272a90ae8cdcb92f3089b4b21
houmin <weihoumin@gmail.com>
2023-01-20 22:52:21 +0800

    feat: update sidebar and layout style

--- assets/js/cosmos.js
+++ assets/js/cosmos.js
@@ -28,6 +28,21 @@ Cosmos.tabs = function () {
     };
 }
 
+
+Cosmos.responsiveTable = function() {
+    const tables = document.querySelectorAll('.post-content table:not(.lntable)');
+    for (let i = 0; i < tables.length; i++) {
+        const table = tables[i];
+        const parentOfParent = table.parentElement.parentElement;
+        if (parentOfParent.className.includes("highlight")) continue;
+        const wrapper = document.createElement('div');
+        wrapper.className = 'table-wrapper';
+        table.parentElement.replaceChild(wrapper, table);
+        wrapper.appendChild(table);
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
     Cosmos.tabs();
+    Cosmos.responsiveTable();
 });
